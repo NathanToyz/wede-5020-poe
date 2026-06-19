ae9e8ac3562866577845152ed23d200c90f12e28
NathanToyz <st10475664@rcconnect.edu.za>
2026-06-19 14:23:50 +0200

    lightbox
    inserted lightbox
    
    inserted lightbox js link on services

--- js script.js
+++ js script.js
@@ -1,1 +1,31 @@
-js/scipt.js
+const searchInput =
+document.getElementById("searchInput");
+
+if(searchInput){
+
+searchInput.addEventListener("keyup", function(){
+
+let filter =
+searchInput.value.toLowerCase();
+
+let cards =
+document.querySelectorAll(".product-card");
+
+cards.forEach(card=>{
+
+let text =
+card.textContent.toLowerCase();
+
+if(text.includes(filter)){
+card.style.display="block";
+}
+
+else{
+card.style.display="none";
+}
+
+});
+
+});
+
+}
